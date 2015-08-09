062878ba8fa76bc68ed4ef09bdd7a2f7c3ba0b6a
Matt Koski <mkoski6@gmail.com>
2015-08-09 16:27:57 -0400

    implements css styles of colors, theme, responsivity, alignment/cleans up scripts.js and adds final game touches and comments/adds screenshots to markdown

--- Readme.md
+++ Readme.md
@@ -7,10 +7,6 @@
   - so I can play TicTacToe against another human player competitively.
 
 
-- As a user, I want to be able to play against an AI,
-  - so I can practice playing TicTacToe and improve my strategies.
-
-
 - As a user, I want to be able to place a marker (X or O) on an empty board space,
   - so I can see where my move has been recorded.
 
@@ -18,13 +14,8 @@
 - As a user, I want to be able to enter my name as player 1 or player 2,
   - so I can distinguish what turn it is or who is winning.
 
-
-- As a user, I want to be able to adjust the game board size to 4x4 or 5x5,
-  - so I can be challenged because 3x3 TicTacToe is for amateurs.
-
-
-- As a user, I want to be able to know who won the game and how many times,
-  - so I know the game has ended and I can keep track of my TicTacToe victories.
+- As a user, I want to know when I win,
+  - so I know that I am a champion of TicTacToe.
 
 
 - As a user, I want to be able to know when I have lost the game,
@@ -38,14 +29,11 @@
 - As a user, I want to be able to reset my game board when the game has completed,
   - so I can play again and again and again and again...
 
-
-- As a user, I want to be able to select my game marker style,
-  - so I can express myself through the art of TicTacToe.
 ----
 
-### TestStatements
+![Image of TicTacToe](screenshots/Desktopview.png)
 
-
+![Image of TicTacToe](screenshots/Mobileview.png)
 
 -----
 ### Technologies
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,4 +1,3 @@
-
 $(function () {
 
     var squares = [];
@@ -8,6 +7,7 @@ $(function () {
     var moves;
     var turn = "X";
 
+          // Winning numbers grid
       /*
        *
        *     273                 84
@@ -21,9 +21,19 @@ $(function () {
        *         73   146   292
        *
        */
-
+          // I add row, column and diagonal sums of squared values to determine the winner
     wins = [7, 56, 448, 73, 146, 292, 273, 84];
 
+//sets player names up on initial load of page, have to reload the game to reset names
+    var setPlayer = function(){
+        var firstplayer = prompt('HHWWHAT is your name, playerrrrr????');
+        var secondplayer = prompt('HHWWHAT is your name, playerrrr????');
+
+        firstplayer = $('#playerone').text(firstplayer);
+        secondplayer = $('#playertwo').text(secondplayer);
+    }
+
+//sets up inital game, places turn on x first (player 1)
     var startNewGame = function() {
         turn = "X";
         score = {"X": 0, "O": 0};
@@ -31,9 +41,9 @@ $(function () {
         squares.forEach(function (square) {
           square.html(empty);
         });
-        console.log(squares);
     }
 
+//determines the win by matching summed value to winning numbers array listed above (win)
     var win = function (score) {
         for (var i = 0; i < wins.length; i += 1) {
             if ((wins[i] & score) === wins[i]) {
@@ -43,6 +53,12 @@ $(function () {
         return false;
     }
 
+//Will set the game board to respond to determined outcome,
+
+//either X is a winner with 3 across in all directions,
+// O is a winner with 3 across in all directions
+// or a draw occurs with no winner determined
+// otherwise allow next turn of X or O
     var set = function() {
 
         if ($(this).html() !== empty) {
@@ -66,13 +82,15 @@ $(function () {
         }
     }
 
+//constructs the board itself by using jquery to create table elements and placing initial css on it
+//appends elements to parents to have the DOM generate the board
      var play = function() {
-        var board = $("<table border=1 cellspacing=0>"), counter = 1;
+        var board = $("<table border=5 border-style=outset bordercolor=white cellspacing=0 color=white align=center display=inline-block>"), counter = 1;
         for (var i = 0; i < size; i += 1) {
             var row = $("<tr>");
             board.append(row);
             for (var j = 0; j < size; j += 1) {
-                var cell = $("<td height=50 width=50 align=center valign=center></td>");
+                var cell = $("<td height=100 width=100 align=center valign=center></td>");
                 cell[0].counter = counter;
                 cell.click(set);
                 row.append(cell);
@@ -81,9 +99,12 @@ $(function () {
             }
         }
 
+//runs player name and places full board on the page and launches a new game instance
+        setPlayer();
         $(document.getElementById("tictactoe") || document.body).append(board);
         startNewGame();
     }
 
+//Time to Tic Tac Toe
     play();
 });
